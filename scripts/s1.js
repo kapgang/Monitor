0fde7766d032cf8adaf3bbed30b5c59977d158b5
Kyle Kaplan <kaplankyle5@gmail.com>
2023-05-10 21:12:56 -0400

    hello

--- scripts/s1.js
+++ scripts/s1.js
@@ -2,19 +2,20 @@ const puppeteer = require('puppeteer');
 const { WebhookClient } = require('discord.js');
 
 const urls = [
-  'https://www.walgreens.com/store/c/cerave-face-and-body-moisturizing-cream-for-normal-to-dry-skin-with-hyaluronic-acid/ID=300396184-product',
+  // 'https://www.walgreens.com/store/c/cerave-face-and-body-moisturizing-cream-for-normal-to-dry-skin-with-hyaluronic-acid/ID=300396184-product',
   'https://www.walgreens.com/store/c/cerave-vitamin-c-face-serum,-skin-brightening-serum-for-face-with-hyaluronic-acid/ID=prod6402924-product',
   'https://www.walgreens.com/store/c/cerave-hydrating-facial-cleanser/ID=300396947-product',
   'https://www.walgreens.com/store/c/cerave-foaming-face-cleanser,-fragrance-free-face-wash-with-hyaluronic-acid/ID=300396945-product',
-  'https://www.walgreens.com/store/c/cerave-pm-facial-moisturizing-lotion-fragrance-free-for-nighttime-use/ID=300396964-product',
+  // 'https://www.walgreens.com/store/c/cerave-pm-facial-moisturizing-lotion-fragrance-free-for-nighttime-use/ID=300396964-product',
   'https://www.walgreens.com/store/c/cerave-am-face-moisturizer-spf-30-oil-free-cream-with-sunscreen/ID=300397121-product',
   'https://www.walgreens.com/store/c/cerave-anti-aging-skin-renewing-night-face-cream-with-hyaluronic-acid/ID=300403254-product',
   'https://www.walgreens.com/store/c/cerave-am-face-moisturizer-spf-30,-oil-free-face-cream-with-sunscreen/ID=300396942-product',
-  'https://www.walgreens.com/store/c/cerave-face-lotion-for-night-with-hyaluronic-acid,-fragrance-free-pm-night-cream/ID=300396660-product',
+  // 'https://www.walgreens.com/store/c/cerave-face-lotion-for-night-with-hyaluronic-acid,-fragrance-free-pm-night-cream/ID=300396660-product',
   'https://www.walgreens.com/store/c/serica-moisturizing-scar-formula/ID=prod6349694-product',
   'https://www.walgreens.com/store/c/cerave-skin-renewing-gel-oil-face-moisturizer-fragrance-free/ID=300396970-product',
   'https://www.walgreens.com/store/c/cerave-renewing-salicylic-acid-body-cream-for-rough-and-bumpy-skin,-fragrance-free/ID=300396968-product',
-  'https://www.walgreens.com/store/c/cerave-sa-body-wash-for-rough-and-bumpy-skin/ID=300396967-product'
+  'https://www.walgreens.com/store/c/cerave-sa-body-wash-for-rough-and-bumpy-skin/ID=300396967-product',
+  'https://www.walgreens.com/store/c/cerave-under-eye-repair-cream-for-dark-circles-and-puffiness,-fragrance-free/ID=300396953-product'
 
 ];
 
