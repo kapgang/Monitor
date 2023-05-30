f06bdda8afec2f4560e5afb39c01fc30fdee8e76
Kyle Kaplan <kaplankyle5@gmail.com>
2023-05-30 16:29:23 -0400

    helloooo

--- scripts/s1.js
+++ scripts/s1.js
@@ -10,13 +10,14 @@ const urls = [
   'https://www.walgreens.com/store/c/cerave-am-face-moisturizer-spf-30-oil-free-cream-with-sunscreen/ID=300397121-product',
   'https://www.walgreens.com/store/c/cerave-anti-aging-skin-renewing-night-face-cream-with-hyaluronic-acid/ID=300403254-product',
   'https://www.walgreens.com/store/c/cerave-am-face-moisturizer-spf-30,-oil-free-face-cream-with-sunscreen/ID=300396942-product',
-  // 'https://www.walgreens.com/store/c/cerave-face-lotion-for-night-with-hyaluronic-acid,-fragrance-free-pm-night-cream/ID=300396660-product',
+  'https://www.walgreens.com/store/c/cerave-face-lotion-for-night-with-hyaluronic-acid,-fragrance-free-pm-night-cream/ID=300396660-product',
   'https://www.walgreens.com/store/c/serica-moisturizing-scar-formula/ID=prod6349694-product',
   'https://www.walgreens.com/store/c/cerave-skin-renewing-gel-oil-face-moisturizer-fragrance-free/ID=300396970-product',
   'https://www.walgreens.com/store/c/cerave-renewing-salicylic-acid-body-cream-for-rough-and-bumpy-skin,-fragrance-free/ID=300396968-product',
   'https://www.walgreens.com/store/c/cerave-sa-body-wash-for-rough-and-bumpy-skin/ID=300396967-product',
   'https://www.walgreens.com/store/c/cerave-under-eye-repair-cream-for-dark-circles-and-puffiness,-fragrance-free/ID=300396953-product',
-  'https://www.amazon.com/Nature-Made-Softgels-Flaxseed-Safflower/dp/B010RS1NBE/ref=sr_1_5?crid=27VEXRUK1ICHL&keywords=B010RS1NBE&qid=1684016260&sprefix=b010rs1nbe%2Caps%2C174&sr=8-5'
+  'https://www.amazon.com/Nature-Made-Softgels-Flaxseed-Safflower/dp/B010RS1NBE/ref=sr_1_5?crid=27VEXRUK1ICHL&keywords=B010RS1NBE&qid=1684016260&sprefix=b010rs1nbe%2Caps%2C174&sr=8-5',
+  'https://www.walgreens.com/store/c/cerave-foaming-face-cleanser,-fragrance-free-face-wash-with-hyaluronic-acid/ID=300396946-product'
   
 
 ];
