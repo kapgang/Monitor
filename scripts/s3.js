508db6dec4c6c6f5878bfec12eb2a463906bd5a5
Kyle Kaplan <kaplankyle5@gmail.com>
2023-06-30 23:18:49 -0400

    hellooo

--- scripts/s3.js
+++ scripts/s3.js
@@ -1,42 +1,62 @@
+const puppeteer = require('puppeteer');
+const { WebhookClient } = require('discord.js');
 
-const axios = require('axios');
+const urls = [
+  'https://www.walgreens.com/store/c/walgreens-regular-strength-antacid-chewable-tablets,-500-mg/ID=prod3252246-product?skuId=sku3251081',
+  'https://www.walgreens.com/store/c/black-girl-sunscreen-spf-30/ID=300425138-product',
+  'https://www.walgreens.com/store/c/walgreens-ephrine-nose-drops-extra-strength/ID=300433946-product',
+  'https://www.walgreens.com/store/c/serica-moisturizing-scar-formula/ID=prod6349694-product'
 
-const affId = 'kk5';  // Fill in your actual Affiliate ID
-const clientId = '12345';  // Fill in your actual Client ID
-const apiKey = 'xegUMalqK0iStKypqHloScm9rJOHJm0v';
-const clientCartId = 'Cart1';  // Generate a unique cart ID
-const skuId = '300396184';  // The product you want to check
-const qty = '1';  // The quantity you want to check
-const type = 'DL';  // The product type
+  
 
-axios.post('https://services-qa.walgreens.com/api/cart/addToCart/v1', {
-    affId,
-    apiKey,
-    clientId,
-    clientCartId,
-    products: [
-        {
-            skuId,
-            qty,
-            type
+];
+
+let currentIndex = 0;
+
+// Create a new webhook client
+const webhookClient = new WebhookClient({ url: 'https://discord.com/api/webhooks/1094877013875105832/OF5iq4putm_y9WSV20Yt5qYXoFHLOMlqvJ_69CES-v5FAGQ2x-L2ZxfTk5pfGPpRJ2s0' });
+
+async function checkStock() {
+  const browser = await puppeteer.launch({ 
+    headless: true,
+    // executablePath: 'C:/Program Files/Google/Chrome/Applications/chrome.exe',
+    // args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+  const page = await browser.newPage();
+
+  while (true) {
+    const url = urls[currentIndex];
+    console.log(`Checking stock for product at ${url}`);
+    await page.goto(url);
+    await page.waitForTimeout(5000);
+
+    try {
+      await page.click('.icon.icon__shipping');
+      await page.waitForTimeout(5000);
+
+      try {
+        await page.click('.icon.icon__shipping');
+        console.log('Product is in stock');
+        
+        // Send message to Discord
+        webhookClient.send(`Product is in stock at ${url}`);
+        
+        urls.splice(currentIndex, 1); // Remove the URL from the list as the product is in stock
+        if (urls.length === 0) {
+          break;
         }
-    ]
-}, {
-    headers: {
-        'Content-Type': 'application/json'
+      } catch (e) {
+        console.log('Product is out of stock');
+      }
+    } catch (e) {
+      console.log('Product is out of stock');
     }
-}).then(response => {
-    const productStatus = response.data.items[0].statusCode;
-    switch (productStatus) {
-        case '000':
-            console.log('Item added to cart with requested quantity');
-            break;
-        case '001':
-            console.log('Item is out of stock');
-            break;
-        default:
-            console.log('Received unknown status code:', productStatus);
-    }
-}).catch(error => {
-    console.error('Failed to check product stock:', error);
-});
+
+    currentIndex = (currentIndex + 1) % urls.length;
+    await page.waitForTimeout(15000); // Wait for 15 seconds before checking the next product
+  }
+
+  await browser.close();
+}
+
+checkStock().catch(console.error);
